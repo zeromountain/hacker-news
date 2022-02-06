741b8592f1aec9153a2928cf29dde774d95e8baf
zeromountain <sonyeongsan@gmail.com>
2022-02-06 18:56:58 +0900

    fetch data

---
+++ app.js
@@ -0,0 +1,24 @@
+const ajax = new XMLHttpRequest();
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+
+ajax.open('GET', NEWS_URL, false);
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');
+
+for (let i = 0; i < newsFeed.length; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
+
+/*
+`<ul>
+  <li>newsFeed[0].title</li>
+  <li>newsFeed[1].title</li>
+  <li>newsFeed[2].title</li>
+</ul>`
+*/
