f08a23340a4b273513c89b35fa8f1ff333121613
zeromountain <sonyeongsan@gmail.com>
2022-02-09 16:14:50 +0900

    hashchange event

--- ReadMe.md
+++ ReadMe.md
@@ -12,3 +12,18 @@
   - `XMLHttpRequest().send()`: URL로 요청 전달
 - JSON
   - `JSON.parse()`: 문자열 형식의 데이터를 Javascript 값이나 객체로 변환
+
+## 2강 주요개념
+
+- 화면 전환
+  - SPA:
+    - 하나의 어플리케이션이 화면을 여러 개를 가지고 전환
+    - 액티브한 화면을 페이지로서 보여줌
+  - 링크를 통한 화면 전환
+- 이벤트: 사용자의 이벤트 사용 시점을 예측하기 어렵기 때문에, 브라우저에서 이벤트 시스템 제공 → 브라우저가 사용자의 이벤트 사용 시점 감지
+  - `EventTarget.addEventListener(이벤트 타입, 실행할 함수)`: 지정한 유형의 이벤트를 대상이 수신할 때마다 호출할 함수를 설정
+    - 함수는 값으로 사용할 수 있기 때문에, 함수의 인자로 사용이 가능
+- 브라우저 객체
+  - location: window 전역 객체의 속성으로, 읽기 전용인 Location 객체를 얻어 올 수 있고, 현재 문서의 URL에 대한 정보를 가짐
+    - `location.hash.substr(1)`: Location 객체의 hash 속성의 값의 첫번째 자리 문자 제거
+    - `CONTENT_URL.replace('@id', id)`: CONTENT_URL에서 @id를 포함한 문자열을 변수 id 값으로 대체
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const container = document.getElementById('root');
+const content = document.createElement('div');
+
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
@@ -7,10 +11,32 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', () => {
+  const id = location.hash.substr(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement('h1');
+
+  title.innerHTML = newsContent.title;
+
+  content.appendChild(title);
+  console.log(newsContent);
+});
+
 for (let i = 0; i < newsFeed.length; i++) {
   const li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement('a');
+  // <li><a></a></li>
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (댓글: ${newsFeed[i].comments_count})`;
+
+  // a.addEventListener('click', () => {})
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
 document.getElementById('root').appendChild(ul);
+document.getElementById('root').appendChild(content);
